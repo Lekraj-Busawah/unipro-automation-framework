18e61f86f26ef9a512f6de14a3c19013293019f3
lekraj.busawah <lekraj.busawah@unipro.io>
2026-02-03 16:07:22 +0400

    Refactor locators in Homepage class for clarity and add is_image_visible method to check image visibility

--- pages/homepage.py
+++ pages/homepage.py
@@ -10,7 +10,7 @@ class Homepage(BasePage):
     locators = {
         # Containers / Sections
         "hero section": (By.CSS_SELECTOR, ".hero-block.block"),
-        "heading": (By.CSS_SELECTOR, "div[class='typography'] h1"),
+        "hero heading": (By.CSS_SELECTOR, "div[class='typography'] h1"),
         "intro feature text": (By.XPATH, "(//div[@class='intro-feature-text'])[1]"),
         "why": (By.CSS_SELECTOR, "#why"),
 
@@ -28,6 +28,8 @@ class Homepage(BasePage):
         "heading": (By.CSS_SELECTOR, "div[class='content-block__typography'] h2"),
         "intro text block": (By.CSS_SELECTOR, "div[class='content-block__typography'] div[class='intro-feature-text']"),
         "call to action": (By.XPATH, "//a[@class='link']"),
+        "why desktop image": (By.CSS_SELECTOR, ".content-block__image.hide-mobile"),
+        "why mobile image": (By.CSS_SELECTOR, ".content-block__image.hide-desktop"),
     }
 
     # ---------------------------------------------------------------------------
@@ -36,3 +38,15 @@ class Homepage(BasePage):
 
     def get_desktop_hero_image(self, visible_image):
         return self.get_element(visible_image)
+    
+    def is_image_visible(self):
+        """
+        Check if image is visible.
+        """
+        try:
+            element = self.wait_for_visibility(self.locators['why'])
+            breakpoint
+            return element.is_displayed()
+        except:
+            return False
+
