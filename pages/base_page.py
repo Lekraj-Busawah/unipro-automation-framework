a1e844a51aec080f7375c45ec99715330411fdad
lekraj.busawah <lekraj.busawah@unipro.io>
2026-03-06 17:58:19 +0400

    Add "get_section_grid_items" method to base_page to improve reusability and maintainability when checking images in grids

--- pages/base_page.py
+++ pages/base_page.py
@@ -209,3 +209,6 @@ class BasePage:
             return element.is_displayed()
         except (TimeoutException, NoSuchElementException):
             return False
+        
+    def get_section_grid_items(self, locator):
+        return self.get_elements(locator)
