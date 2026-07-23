e8414ddb323183aadd5b9d08e18fdd5323ed0105
Lekraj-Busawah <lekraj.busawah@unipro.io>
2026-07-23 17:31:12 +0400

    Add initial step definitions for "What We Do" page

---
+++ features/steps/what_we_do_steps.py
@@ -0,0 +1,7 @@
+from behave import *
+from behave.exception import StepNotImplementedError
+from pages.what_we_do import WhatWeDo
+
+# ---------------------------------------------------------------------------
+# HERO SECTION
+# ---------------------------------------------------------------------------
