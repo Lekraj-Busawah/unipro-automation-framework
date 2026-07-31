94c5fdced8b36cf1de38ae85df2f0fdec7b73abb
Lekraj-Busawah <lekraj.busawah@unipro.io>
2026-07-31 17:51:54 +0400

    Refactor common_steps.py to include Homepage in PAGE_REGISTRY and improve navigation handling

--- features/steps/common_steps.py
+++ features/steps/common_steps.py
@@ -1,4 +1,5 @@
 from behave import *
+from pages.homepage import Homepage
 from pages.who_we_are import WhoWeAre
 from pages.what_we_do import WhatWeDo
 
@@ -10,6 +11,7 @@ from pages.what_we_do import WhatWeDo
 PAGE_REGISTRY = {
     "/who-we-are/": WhoWeAre,
     "/what-we-do/": WhatWeDo,
+    "/": Homepage,
 }
 
 @given(u'the user navigates to "{path}"')
@@ -17,3 +19,12 @@ def step_impl(context, path):
     page_class = PAGE_REGISTRY[path]
     context.current_page = page_class(context.driver)
     context.current_page.navigate_to_url(path)
+
+
+@then(u'the {element_name} is visible and contains "{element_contains}"')
+def step_impl(context, element_name, element_contains):
+    is_visible = context.current_page.is_element_displayed(element_name)
+    assert is_visible is True, f"The {element_name} container was not visible on the page!"
+    actual_text = context.current_page.get_element_text(element_name)
+    assert element_contains in actual_text, f"Expected text to contain '{element_contains}' but found {actual_text}"
+
