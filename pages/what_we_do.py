b32c7d0716057ce24b1e65c59cc78986183169d0
Lekraj-Busawah <lekraj.busawah@unipro.io>
2026-08-03 17:41:32 +0400

    Add locators for 'Our Promise', 'Unipro Difference' and 'Final CTA' sections in what_we_do.py

--- pages/what_we_do.py
+++ pages/what_we_do.py
@@ -28,4 +28,27 @@ class WhatWeDo(BasePage):
         "build vs buy desktop image": (By.XPATH, "//div[@id='transformation']//img[contains(@class,'hide-mobile')][1]"),
         "build vs buy mobile image": (By.XPATH, "//div[@id='transformation']//img[contains(@class,'hide-desktop')][1]"),
 
+        # Our Promise Section
+        "our promise eyebrow text": (By.XPATH, "//p[normalize-space()='Our promise']"),
+        "our promise heading": (By.XPATH, "(//*[@id='services']//h2)[1]"),
+        "our promise intro text block": (By.XPATH, "//div[contains(p, 'three essential outcomes')]"),
+        "our promise list": (By.XPATH, "//*[@id='services']//ul//p"),
+        "our promise list item": (By.XPATH, "(//*[@id='services']//ul//p)[{position}]"),#
+        "our promise contact CTA": (By.XPATH, "(//a[@class='button button--primary'][normalize-space()='Contact Us'])[1]"),
+        "our promise desktop image": (By.XPATH, "(//*[@id='services']//img[contains(@class,'hide-mobile')])[1]"),
+        "our promise mobile image": (By.XPATH, "(//*[@id='services']//img[contains(@class,'hide-desktop')])[1]"),
+
+        # Unipro Difference Section
+        "unipro difference eyebrow text": (By.XPATH, "//p[normalize-space()='Our Approach: Human-Led, AI Accelerated Delivery']"),
+        "unipro difference heading": (By.XPATH, "//h2[normalize-space()='The Unipro Difference']"),
+        "unipro difference intro text block": (By.XPATH, "(//div[contains(@class,'intro-feature-text')])[4]"),
+        "unipro difference contact CTA": (By.XPATH, "(//a[@class='button button--primary'][normalize-space()='Contact Us'])[3]"),
+        "unipro difference desktop image": (By.XPATH, "(//*[@id='services']//img[contains(@class,'hide-mobile')])[2]"),
+        "unipro difference mobile image": (By.XPATH, "(//*[@id='services']//img[contains(@class,'hide-desktop')])[2]"),
+
+        # Final CTA Section
+        "final cta eyebrow text": (By.XPATH, "//p[contains(text(),'Ready to Liberate your business')]"),
+        "final cta heading": (By.XPATH, "//h2[contains(text(),'Talk to our team to build a bespoke solution')]"),
+        "final cta intro text block": (By.XPATH, "//p[contains(text(),'No pressure, no commitment')]"),
+        "final cta contact CTA": (By.XPATH, "(//a[@class='button'])[1]"),
     }
