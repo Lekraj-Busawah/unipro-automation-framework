4a270b37e148c388b5b593152d20ab6de95eae3e
Lekraj-Busawah <lekraj.busawah@unipro.io>
2026-08-03 17:43:34 +0400

    Remove what_we_do_steps.py as it is no longer needed

--- features/steps/what_we_do_steps.py
+++
@@ -1,7 +0,0 @@
-from behave import *
-from behave.exception import StepNotImplementedError
-from pages.what_we_do import WhatWeDo
-
-# ---------------------------------------------------------------------------
-# HERO SECTION
-# ---------------------------------------------------------------------------
