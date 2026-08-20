aa412566e642de465b4c396b6b155d5e31a94b85
Lekraj-Busawah <lekraj.busawah@unipro.io>
2026-08-20 14:08:02 +0400

    Add locators for hero desktop and mobile images in Events page

--- pages/events.py
+++ pages/events.py
@@ -15,5 +15,6 @@ class Events(BasePage):
 
        # Hero section
         "hero heading": (By.CSS_SELECTOR, ".hero-block .typography h1"),
-      
+        "hero desktop image": (By.CSS_SELECTOR, ".hero-block__image.hide-mobile"),
+        "hero mobile image": (By.CSS_SELECTOR, ".hero-block__image.hide-desktop"),
     }
